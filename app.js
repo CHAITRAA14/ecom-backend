ca37f8870d0cd7799f12433d6704e1da0b0c6e34
CHAITRAA14 <chaitranalawad@gmail.com>
2025-03-19 11:35:21 +0530

    final change

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const {User} = require('./model/User');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
-const bcrypt = require('bcrypt');
+const bcryptjs = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 const {Product} = require('./model/Product');
 const {Cart} = require('./model/Cart');
@@ -14,7 +14,11 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'))
 
-mongoose.connect('mongodb://127.0.0.1:27017/kleProject')
+//VX5CbcdhFJrmBJU6-password
+
+let MONGODB_URL = "mongodb+srv://chaitranalawad:VX5CbcdhFJrmBJU6@cluster0.fte30.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(MONGODB_URL)
 .then(()=>{
     console.log("DB is connected")
 }).catch((err)=>{
